01479d495352baaaaaa4e7dc8a974920e525bc4f
Maria Xia <maria.xia@gmail.com>
2017-01-16 18:54:39 -0500

    added volleyball

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const volleyball = require('volleyball');
+
+app.use('/', volleyball);
+
+// app.use('/', function(req, res, next){
+//   console.log(req.method, req.url, res.statusCode);
+//   next();
+// });
 
 app.use('/:page', function(req, res, next){
-  if (req.params.page)
-    res.send("that page doesn't exist yet!");
+  if (req.params.page === 'special')
+    res.send("You've reached the special area.");
+  else {
+    res.send("That page doesn't exist yet!")
+  }
 });
+
 app.get('/', function(req, res){
   res.send('Welcome!');
 });
